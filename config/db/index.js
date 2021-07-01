4cd0dedfb9a4585b53cba305f6c0b84a2df7865d
vhung0206 <vhung0206@gmail.com>
2021-07-02 03:40:24 +0700

    perfect

--- config/db/index.js
+++ config/db/index.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('u705797778_HTT3C', 'root', '', {
-    host: '127.0.0.1',
+const sequelize = new Sequelize('u705797778_HTT3C', 'u705797778_vanhung0206', 'Hung0327712600', {
+    host: '156.67.222.169',
     dialect: 'mysql'
 });
 
 module.exports=sequelize;
+
